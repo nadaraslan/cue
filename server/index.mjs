be0e2d6b65061cc5ea1c7063c7c5bd4a6752f6bb
Nada Raslan <nadamraslan@gmail.com>
2026-03-29 16:22:01 +0400

    Fix PDF worker MIME type for production

--- server/index.mjs
+++ server/index.mjs
@@ -20,6 +20,7 @@ const contentTypes = {
   ".css": "text/css; charset=utf-8",
   ".html": "text/html; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
+  ".mjs": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
   ".png": "image/png",
   ".svg": "image/svg+xml",
